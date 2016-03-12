2a36de12b89bd0194febb2ed5657a51efa156052
Fajar Sodik <my@fsodic.com>
2016-03-12 04:43:25 -0800

    Update index.php

--- index.php
+++ index.php
@@ -6,31 +6,12 @@ Author: Fajar Sodik
 Blog: www.fsodic.com
 Facebook: www.fb.com/presiden.fajar
 */
-
-include ('./fajarsodik.php');
-
-$fs = isset($_GET['fs']) ? trim($_GET['fs']) :'';
-switch ($fs)
+if(file_exists('./inc/blog.php'))
 {
-default:
-include  
-('./tema/index.php');
-break;
-
-case 'post':
-include ('./tema/post.php');
-break;
-
-case 'page':
-include ('./tema/page.php');
-break;
-
-case 'category':
-include ('./tema/category.php');
-break;
-
-case '404':
-include ('./tema/404.php');
-break;
+include ('./inc/blog.php');
+}
+else
+{
+header ('Location: /install.php');
 }
 ?>
